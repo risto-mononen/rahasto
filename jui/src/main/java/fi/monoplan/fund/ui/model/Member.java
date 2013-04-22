f3e3d2fa05f057795dc5c346f76463db02c599a6
Risto Mononen <risto.mononen@notava.com>
2013-04-22 14:53:42 +0300

    Stub for change from h2 to mysql

--- jui/src/main/java/fi/monoplan/fund/ui/model/Member.java
+++ jui/src/main/java/fi/monoplan/fund/ui/model/Member.java
@@ -1,93 +1,126 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package fi.monoplan.fund.ui.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
 
 @SuppressWarnings("serial")
 @Entity
 @XmlRootElement
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class Member implements Serializable {
 
-    @Id
-    @GeneratedValue
-    private Long id;
+	@Id
+	@GeneratedValue
+	private Long id;
 
-    @NotNull
-    @Size(min = 1, max = 25)
-    @Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
-    private String name;
+	private String fund;
+	private String rating;
+	private String currency;
+	@Column(columnDefinition = "DECIMAL(12,4)")
+	private BigDecimal nav;
+	@Column(columnDefinition = "DECIMAL(12,4)")
+	private BigDecimal day1;
+	@Column(columnDefinition = "DECIMAL(12,4)")
+	private BigDecimal month3;
+	@Column(columnDefinition = "DECIMAL(12,4)")
+	private BigDecimal month6;
+	@Column(columnDefinition = "DECIMAL(12,4)")
+	private BigDecimal year1;
+	@Column(columnDefinition = "DECIMAL(12,4)")
+	private BigDecimal year3;
+	private String date;
 
-    @NotNull
-    @NotEmpty
-    @Email
-    private String email;
+	public Long getId() {
+		return id;
+	}
 
-    @NotNull
-    @Size(min = 10, max = 12)
-    @Digits(fraction = 0, integer = 12)
-    @Column(name = "phone_number")
-    private String phoneNumber;
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public String getFund() {
+		return fund;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setFund(String fund) {
+		this.fund = fund;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getRating() {
+		return rating;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getCurrency() {
+		return currency;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+	public BigDecimal getNav() {
+		return nav;
+	}
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+	public void setNav(BigDecimal nav) {
+		this.nav = nav;
+	}
+
+	public BigDecimal getDay1() {
+		return day1;
+	}
+
+	public void setDay1(BigDecimal day1) {
+		this.day1 = day1;
+	}
+
+	public BigDecimal getMonth3() {
+		return month3;
+	}
+
+	public void setMonth3(BigDecimal month3) {
+		this.month3 = month3;
+	}
+
+	public BigDecimal getMonth6() {
+		return month6;
+	}
+
+	public void setMonth6(BigDecimal month6) {
+		this.month6 = month6;
+	}
+
+	public BigDecimal getYear1() {
+		return year1;
+	}
+
+	public void setYear1(BigDecimal year1) {
+		this.year1 = year1;
+	}
+
+	public BigDecimal getYear3() {
+		return year3;
+	}
+
+	public void setYear3(BigDecimal year3) {
+		this.year3 = year3;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
 }
