6eae26e5178b19cc5c88028da0f5ef12c9fbe26b
Risto Mononen <leuka.mononen@gmail.com>
2013-04-22 07:42:46 +0000

    Loop all arg files in import script

--- load/import.sh
+++ load/import.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-mysqlimport -u root rahasto `pwd`/$1 --fields-terminated-by=',' --columns=fund,rating,currency,nav,day1,month3,month6,year1,year3,date --local
+for file in "$@"
+do
+    mysqlimport -u root rahasto `pwd`/$file --fields-terminated-by=',' --columns=fund,rating,currency,nav,day1,month3,month6,year1,year3,date --local
+done
