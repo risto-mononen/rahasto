5f55a6875a95748c4e32a6c5474e9e60635100a6
Risto Mononen <risto.mononen@notava.com>
2013-04-23 09:42:53 +0300

    Add Bond db CRUD

---
+++ jui/src/test/java/fi/monoplan/fund/ui/test/BondRegistrationTest.java
@@ -0,0 +1,67 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fi.monoplan.fund.ui.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.math.BigDecimal;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import fi.monoplan.fund.ui.model.Bond;
+import fi.monoplan.fund.ui.service.BondRegistration;
+import fi.monoplan.fund.ui.util.Resources;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class BondRegistrationTest {
+    @Deployment
+    public static Archive<?> createTestArchive() {
+        return ShrinkWrap.create(WebArchive.class, "test.war")
+                .addClasses(Bond.class, BondRegistration.class, Resources.class)
+                .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                // Deploy our test datasource
+                .addAsWebInfResource("test-ds.xml");
+    }
+
+    @Inject
+    BondRegistration bondRegistration;
+
+    @Inject
+    Logger log;
+
+    @Test
+    public void testRegister() throws Exception {
+        Bond newBond = new Bond();
+        newBond.setNav(new BigDecimal(1234.5678));
+        newBond.setCurrency("EUR");
+        newBond.setDate("22.4.");
+        bondRegistration.register(newBond);
+        assertNotNull(newBond.getId());
+        log.info(newBond.getFund() + " was persisted with id " + newBond.getId());
+    }
+
+}
